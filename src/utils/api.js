6b3cfade534259828e187e836d44a3c44dac0304
Bharat bhushan <43534123+bharat6138@users.noreply.github.com>
2023-02-25 13:17:04 +0530

    Add files via upload

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,11 +5,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
 	params: { hl: "en", gl: "IN" },
 	headers: {
-		"X-RapidAPI-Key":
-			"5cd2abdd35msheba91cf99362cfbp1cfbb9jsn6b79f840e82d" ||
-			"4902351b37msh1f3b2f72569b3a9p1fe51cjsnbbde08e1d334" ||
-			"4596dbf03emsh3aeb8527c864ba3p1334bbjsn91fa17943875" ||
-			"197f2a7ae9mshddf80f39bdc12d5p100edcjsn75e7ca47fa4c",
+		"X-RapidAPI-Key": "197f2a7ae9mshddf80f39bdc12d5p100edcjsn75e7ca47fa4c",
 		// "5cd2abdd35msheba91cf99362cfbp1cfbb9jsn6b79f840e82d" ||
 		// "4902351b37msh1f3b2f72569b3a9p1fe51cjsnbbde08e1d334" ||
 		// "4596dbf03emsh3aeb8527c864ba3p1334bbjsn91fa17943875", ||
